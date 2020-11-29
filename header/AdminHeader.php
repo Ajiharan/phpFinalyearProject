249f1a3fc63b74d34b7f515c1881aaf56b41d008
Ajiharan <ajiharan@unicomtechnology.co.uk>
2020-11-29 12:13:38 +0530

    update admin home ui

--- header/AdminHeader.php
+++ header/AdminHeader.php
@@ -12,7 +12,11 @@
     
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">CMS</a>
+  <a class="navbar-brand" href="#">
+  <?php
+   echo "<img src='images/logo.png' class='header__logo'>"; 
+?>
+  </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
