b2a8164a74529ff596ad7b51211858d64c5c5922
Ajiharan <ajiharan@unicomtechnology.co.uk>
2020-12-08 22:27:45 +0530

    flixed issue

--- server/DeleteOther.php
+++ server/DeleteOther.php
@@ -12,7 +12,15 @@
         $res->execute([$id]);
         $tot=$res->rowCount();
         if($tot >0){
-            echo "Deleted";
+            $sql1="delete from expenses where tid=?";
+            $res1=$con->prepare($sql1);
+            $res1->execute([$tid]);
+            $tot1=$res1->rowCount();
+            if($tot1 >0){
+                echo "Deleted";
+            }else{
+                echo "cannot delete";
+            }
         }else{
             echo "cannot delete";
         }
